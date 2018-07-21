57652ab0d936f1bb19e2b54972554b6254297f9c
paypulse <sourcedev5@gmail.com>
2018-07-22 08:06:22 +0900

    20180722_studyHobby

---
+++ springEx_5_DI4_Pencil/src/main/java/com/javalec/ex/Pencil4B.java
@@ -0,0 +1,12 @@
+package com.javalec.ex;
+
+public class Pencil4B implements Pencil {
+
+	@Override
+	public void Use() {
+		// TODO Auto-generated method stub
+		System.out.println("4B 입니다.");
+
+	}
+
+}
