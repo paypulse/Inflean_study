e59788b315a198303d99fb8e12355f66fdd9b62d
paypulse <sourcedev5@gmail.com>
2018-07-23 04:37:38 +0900

    20180723

---
+++ springEx_11_Environment02/src/main/java/com/javalec/ex/MainClass.java
@@ -0,0 +1,25 @@
+package com.javalec.ex;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AbstractApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationCTX.xml");
+		
+		AdminConnection connection = ctx.getBean("adminConnection" , AdminConnection.class);
+		System.out.println("adminId :" + connection.getAdminId());
+		System.out.println("adminPw :" + connection.getAdminPw());
+		System.out.println("sub_adminID :" + connection.getSub_adminId());
+		System.out.println("sub_adminPW :" + connection.getSub_adminPw());
+		
+		
+		
+		ctx.close();
+		
+	}
+
+}
